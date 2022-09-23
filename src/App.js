311a6adfb23bde324d3b850f096bc694326404da
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-09-23 12:06:51 +0600

    use state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <LoadCountries></LoadCountries>
     </div>
   );
 }
 
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Visiting Every country of the world!!!</h1>
+      <h3>Available Countries : {countries.length}</h3>
+    </div>
+  );
+}
+//! https://restcountries.com/v3.1/all
 export default App;
